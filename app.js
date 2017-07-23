41a7144840171921978a90b2c8f21a6b2a2add11
noorahmad <noorahmad23@hotmail.com>
2017-07-23 11:33:50 -0700

    added SHOW route

--- app.js
+++ app.js
@@ -50,6 +50,17 @@ app.post("/blogs", (req, res) => {
   })
 })
 
+//Show Route
+app.get('/blogs/:id', (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect('/blogs')
+    } else {
+        res.render('show', {blog: foundBlog})
+    }
+  })
+})
+
 app.listen(3000, () => {
   console.log('listening on 3000')
 })
